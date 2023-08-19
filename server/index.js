11f7b36944bad65a96902f106c0512d9bc83b342
Akram-Star-Coder <elbasria31@gmail.com>
2023-08-19 03:19:23 +0100

    sdw

--- server/index.js
+++ server/index.js
@@ -4,6 +4,11 @@ const dotenv = require('dotenv');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const cors = require('cors');
+const bodyParser = require('body-parser')
+const clientRoutes = require('./Routes/clientRoutes');
+const generalRoutes = require('./Routes/generalRoutes');
+const managementRoutes = require('./Routes/managementRoutes');
+const salesRoutes = require('./Routes/salesRoutes');
 
 
 const app = express();
@@ -14,9 +19,17 @@ app.use(express.json());
 app.use(cors());
 app.use(helmet());
 app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
 
 
 app.listen(process.env.PORT, ()=>{
     //calback function when success
     console.log('Connected to Server');
 });
+
+
+app.use('/client', clientRoutes);
+app.use('/general', generalRoutes);
+app.use('/management', managementRoutes);
+app.use('/sales', salesRoutes);
